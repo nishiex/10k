1f810917a55dc9bb767d8c400e8bb7079e3ff708
nishiex <nishitpatel.expertwise@gmail.com>
2025-04-13 23:43:51 +0530

    updated  vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://madhav-battery.onrender.com',
+        changeOrigin: true
+      }
+    }
+  }
 })
